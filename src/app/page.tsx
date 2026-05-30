ce5c71c0656d48700ad2f6f921b3319327bdc03f
feijaoozin <gonardo1234@gmail.com>
2026-05-30 08:02:58 -0300

    testando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ import imagem from "./imagem.jpeg";
 export default function Home() {
   return (
     <main className="relative h-screen w-screen">
-      <Image src={imagem} alt="Imagem" fill priority className="object-cover" />
+      <Image
+        src={imagem}
+        alt="Imagem"
+        fill
+        priority
+        className="object-contain"
+      />
 
       <div className="absolute inset-0 flex items-center justify-center bg-black/50">
         <div className="" />
